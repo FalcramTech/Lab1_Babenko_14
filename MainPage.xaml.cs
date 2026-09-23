72b22807725f977a9f27b5c5a367f72fd3cc2b19
FalcramTech <vanyababenko2014@gmail.com>
2026-09-23 21:23:53 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using StudentApp.ViewModels;
+
+namespace StudentApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new StudentViewModel();
+        }
+    }
+}
